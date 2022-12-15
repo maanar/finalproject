77af31832a7b9701e907915b14a3c6d4dd95a335
manar ashraf <ashrafmanar151@gmail.com>
2022-12-15 19:58:57 +0200

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import data from "./data";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+  <div>
+      <header >
+        <a href="/">final project</a>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}/>
+
+        </Routes>
+      <h1> featured products</h1>
+      
+    <div className= "products">
+        
+        {data.products.map(product => (
+        <div className="product" key ={product.slug}>
+          <a href ={`/product/${product.slug}`}>
+          <img src ={product.image} alt={product.name}/>
+          </a>
+          <div className="product-info">
+          <a href ={`/product/${product.slug}`}>
+          <p> {product.name} </p>
+          </a>
+          <p> <strong>{product.price} </strong></p>
+          <button> Add to cart</button>
+          </div>
+        </div>))
+      }
+      
+    </div>
+      </main>
+  </div>
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+const data ={
+    products: [
+        {
+          name: 'strawbery milk',
+          slug: 'strawbery-milk',
+          category: 'milks',
+          image: '/images/p1.png', // 679px × 829px
+          price: 14,
+          countInStock: 1,
+          brand: 'dina',
+          rating: 4.5,
+          numReviews: 10,
+          description: 'high quality ',
+        },
+        {
+            name: 'banana milk',
+            slug: 'banana-milk',
+            category: 'milks',
+            image: '/images/p3.png', // 679px × 829px
+            price: 14,
+            countInStock: 1,
+            brand: 'dina',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality ',
+          }, {
+            name: 'chocolate milk',
+            slug: 'chocolate-milk',
+            category: 'milks',
+            image: '/images/p2.png', // 679px × 829px
+            price: 14,
+            countInStock: 1,
+            brand: 'dina',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality ',
+          },
+      ],
+}
+export default data ;
